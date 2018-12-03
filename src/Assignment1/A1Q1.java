276bceb7b562c145ee43907a1cb7e8e26a15dc31
Mr. Lamont <adam_lamont@wrdsb.ca>
2018-12-03 13:18:08 -0500

    Added first drawing with some animation

--- src/Assignment1/A1Q1.java
+++ src/Assignment1/A1Q1.java
@@ -17,7 +17,7 @@ public class A1Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
+
 }
